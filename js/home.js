121c37aa47e72152b832baa6a5b2082d3a969bd4
Wixieyy <shendrickwilliams@gmail.com>
2024-03-17 22:05:02 +0100

    links and animations fixed, about page created

---
+++ js/home.js
@@ -0,0 +1,16 @@
+document.getElementById("aboutuslink").addEventListener("click", function(event) {
+    // Prevent the default behavior of the hyperlink
+    event.preventDefault();
+
+    // Add fade-out class to trigger the animation
+    document.querySelector('.center-box').classList.add('fade-out');
+
+    // Delay before navigating to the URL
+    setTimeout(function() {
+        // Get the URL from the href attribute
+        var url = document.getElementById("aboutuslink").getAttribute("href");
+
+        // Navigate to the URL
+        window.location.href = url;
+    }, 250); // Adjust the delay time (in milliseconds) to match the duration of the animation
+});
